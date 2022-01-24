9761747e181c5fe4cda20c1b4cde841ef8a35505
dean <dean@odeanrobotics.com>
2022-01-24 20:20:38 +0900

    npm install vuetify

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify);
 // import dictionary from './model/dictionary.js';
 
 Vue.config.productionTip = false
@@ -10,5 +15,7 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  vuetify: new Vuetify(),
   render: h => h(App)
+  // itender: h => h(App)
 }).$mount('#app')
